af8ec05e0d6651f8b41a424aa63d6e3aa9e91c3c
alexsa2517 <admoney4@gmail.com>
2026-08-12 17:36:53 +0700

    feat: launch production studio workflow as frontend entrypoint

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import ProductionStudio from './pages/ProductionStudio';
+import './production-studio.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ProductionStudio />
   </React.StrictMode>
 );
